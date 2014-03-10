d2a53e95f8f7026a4f59bb756cedd025d3fa6be9
Michael Leber <mikeleber117@gmail.com>
2014-03-09 19:21:10 -0600

    - Initial commit for project.

---
+++ RAC-Example/RAC-Example/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RAC-Example
+//
+//  Created by Michael Leber on 3/9/14.
+//  Copyright (c) 2014 Michael Leber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RAC-Example/RAC-Example/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  RAC-Example
+//
+//  Created by Michael Leber on 3/9/14.
+//  Copyright (c) 2014 Michael Leber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
